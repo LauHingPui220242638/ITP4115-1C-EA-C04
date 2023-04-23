8f9e3469f7532db5313807f3d360e374f81f70a5
LauHingPui220242638 <113015644+LauHingPui220242638@users.noreply.github.com>
2023-04-23 10:49:27 +0000

    change test_forum

--- test_forum.py
+++ test_forum.py
@@ -12,16 +12,25 @@ u1 = User(username='Ben', email='ben@example.com')
 u2 = User(username='May', email='may@example.com')
 u3 = User(username='Tim', email = 'time@code.com')
 u4 = User(username='ForumAdmin', email = 'forumadmin@code.com')
+u4.is_admin = True
 u1.set_password("Ben")
 u2.set_password("May")
 u3.set_password('Time')
 u4.set_password('ForumAdmin')
 db.session.add_all([u1,u2,u3,u4])
+db.session.commit()
+
 
 #Forum Category
-fc1 = ForumCat(color= "FF00FF",forumcatsname = "HQ Announcements", description = "Here is where you’ll find the latest updates from The Codecademy Team. We will announce things like new product features, curriculum releases, and even site outages.")
-fc2 = ForumCat(color= "5500FF",forumcatsname = "Getting Started", description = "New to the Forums? Orient yourself, get the tools, and learn the rules that will help make these Forums a helpful resource on your learning journey.")
-fc3 = ForumCat(color= "5F50F5",forumcatsname = "Get Help", description = "Ask questions, get help with an exercise, and chat about your Codecademy coursework here. Click in for language-specific topic pages.")
+fc1 = ForumCat(color= "A432FF",forumcatsname = "HQ Announcements", description = "Here is where you’ll find the latest updates from The Codecademy Team. We will announce things like new product features, curriculum releases, and even site outages.")
+fc2 = ForumCat(color= "5527F2",forumcatsname = "Getting Started", description = "New to the Forums? Orient yourself, get the tools, and learn the rules that will help make these Forums a helpful resource on your learning journey.")
+fc3 = ForumCat(color= "5C50F5",forumcatsname = "Get Help", description = "Ask questions, get help with an exercise, and chat about your Codecademy coursework here. Click in for language-specific topic pages.")
+
+db.session.add_all([
+    fc1,fc2,fc3,
+])
+db.session.commit()
+
 
 
 p1 = Topic(author_id=1, forumcat_id=1, title="If you could do anything",)
@@ -29,10 +38,13 @@ p2 = Topic(author_id=2, forumcat_id=1, title="Learn ChatGPT with Codecademy!",)
 p3 = Topic(author_id=3, forumcat_id=1, title="Spread Sheet Logic",)
 p4 = Topic(author_id=4, forumcat_id=1, title="Big announcement!",)
 
+db.session.add_all([
+    p1,p2,p3,p4
+])
+db.session.commit()
 
 
-
-r1 = Reply.func_reply("""
+r1 = Reply.create_reply("""
 Hey y’all!
 
 In an exciting new milestone for Codecademy, I’m proud to share our first ever TV commercial, inspired by many of the stories your fellow learners have shared right here in the community.
@@ -42,17 +54,17 @@ YB
 I hope this continues to inspire you to unlock your full potential and continue your learning journey. Keep up the amazing work!
 """,1,1,0)
 
-r2 = Reply.func_reply("""
+r2 = Reply.create_reply("""
 Well done CodeCademy, TV Commercial is on fire!!
 I am active subscriber since 2020, closing the gap to my first 100 certificates, a working father with two lovely kids, always eager to learn, Codecademy offers high quality courses that continuously getting new material with new ideas, concepts and techniques, adapting every day in a rapidly changing era of technology, there is no better way to stay in top shape with cutting edge knowledge, a solid solution with a variety of choices to pick and start your new daily habit that will transform you, in a better version of you. Here is my LinkedIn profile, connect with me to celebrate together my first topic, when I reach my first 100 certificates.!!! (at 91 and going up!!)
 https://www.linkedin.com/in/ioannis-e-kommas-6a8004a6/ 32
 """,1,2,0)
 
-r3 = Reply.func_reply("""
+r3 = Reply.create_reply("""
 So, if you want to have a career as a Web Programmer, you have to understand programming languages, at least you have to master the programming languages ​​that are often used.
 """,1,3,2)
 
-r4 = Reply.func_reply("""
+r4 = Reply.create_reply("""
 :fire: Wow!
 Great work Codecademy Team
 """,1,1,2)
@@ -68,12 +80,13 @@ p1.tags.append(t2)
 p1.tags.append(t3)
 p1.tags.append(t4)
 
+
+
+
 db.session.add_all([
-    fc1,fc2,fc3,
-    p1,p2,p3,p4,
     r1,r2,r3,r4,
     t1,t2,t3,t4
 ])
 
-
 db.session.commit()
+

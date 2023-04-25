a4beffa4409de15fff645b0227261f845b655f98
Ryanwongct-hk <113015336+Ryanwongct-hk@users.noreply.github.com>
2023-04-26 01:14:31 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -43,7 +43,7 @@ if not app.debug:
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+    file_handler = RotatingFileHandler('logs/codeacdemy.log', maxBytes=10240,
                                        backupCount=10)
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
